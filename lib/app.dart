fa81113b42dcd733003d427463635d7d50fe6337
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-23 09:53:14 -0300

    meu arquivo app.dart veio só as rotas, no local service veio sem muitas funções, como o setPromptsuccess, o localpath, setlocalPath etc

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,30 @@
-routes: {
-  '/config': (_) => ConfigPage(),
-  '/prompt': (_) => PromptPage(),
-  '/loading': (_) => LoadingPage(),
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  final bool hasConfig;
+
+  App({required this.hasConfig});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: hasConfig ? '/prompt' : '/config',
+      routes: {
+        '/config': (_) => ConfigPage(),
+        '/prompt': (_) => PromptPage(),
+        '/loading': (_) => LoadingPage(),
+        '/terminal': (_) => TerminalPage(),
+      },
+      theme: ThemeData(
+        primaryColor: Colors.blueAccent,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+        ),
+      ),
+    );
+  }
 }
