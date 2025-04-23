8cb31196cdb09406b501e6ceb7f0861e8c37ac83
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-23 09:40:10 -0300

    No lugar de ser um chat, transfome numa tela que tem o input de texto e o botão de enviar, ambos centralizado nomeio da tela. O input de texto deve permitir varias linhas. Ao enviar para api, a tela muda para uma tela de carregamento. Caso a resposta da api seja 200. A tela muda e faz um clone do repositorio contido no github usando o terminal, este pocesso deve mostrar na tela o carregamento e execução dos comandos. A pasta onde fica armazenado deve ser definida na tela inicial. Outra alteração da tela inicial é o owner que está como dropdown e é campo de texto simples, coloca para buscar assim que o usuario clicar fora dele

---
+++ lib/features/loading/page/loading_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatefulWidget {
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  late final LoadingController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = Provider.of<LoadingController>(context, listen: false);
+    controller.executeClone().then((_) => Navigator.pushNamed(context, '/terminal'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [Color(0xFF2980b9), Color(0xFF3498db)]),
+        ),
+        child: Center(
+          child: CircularProgressIndicator(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
