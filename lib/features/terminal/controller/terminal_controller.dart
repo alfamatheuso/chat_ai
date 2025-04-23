680d9c2a5581f61752405f8a609d64ce69101e8d
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-23 10:39:27 -0300

    preciso adicionar a tela de terminal, para mostrar o que está sendo executado

---
+++ lib/features/terminal/controller/terminal_controller.dart
@@ -0,0 +1,19 @@
+import 'package:mobx/mobx.dart';
+part 'terminal_controller.g.dart';
+
+class TerminalController = _TerminalControllerBase with _$TerminalController;
+
+abstract class _TerminalControllerBase with Store {
+  @observable
+  ObservableList<TerminalLog> logs = ObservableList<TerminalLog>();
+
+  @action
+  void addLog(String message) {
+    logs.insert(0, TerminalLog(message: message, timestamp: DateTime.now()));
+  }
+
+  @action
+  void clearLogs() {
+    logs.clear();
+  }
+}
